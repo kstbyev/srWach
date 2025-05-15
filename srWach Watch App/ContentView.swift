3989e19d46575d798f36eaad085d1cd50fa764a8
kstbyev <madi2005@bk.ru>
2025-05-15 23:00:03 +0500

    Initial Commit

---
+++ srWach Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  srWach Watch App
+//
+//  Created by Madi Sharipov on 15.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ srWach Watch App/srWachApp.swift
@@ -0,0 +1,17 @@
+//
+//  srWachApp.swift
+//  srWach Watch App
+//
+//  Created by Madi Sharipov on 15.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct srWach_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
